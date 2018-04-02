0af1e52851e604f586063d69ab53c5ee7ebb3cc6
hogepiyo334 <haruki.tokyo.ct.1013@gmail.com>
2018-04-02 18:04:14 +0900

    firstcommit

---
+++ template/template.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(0,OUTPUT);
+   pinMode(1,OUTPUT);
+   pinMode(2,OUTPUT);
+   pinMode(3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(0,HIGH);
+  delay(1000);
+}
